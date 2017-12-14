1939b6cef39c5d442d29f4de5e69f3dac69874da
Luke <luke.igel@gmail.com>
2017-12-14 17:16:32 -0600

    can now parse .txt for names and ids

--- attendance.py
+++ attendance.py
@@ -2,46 +2,52 @@ import time
 import datetime
 
 
-global names
-names = ["Jackathon", "Boomer Kingston", "Paul Deangelo-Willis"]
+
+
+#names = ["Jackathon", "Boomer Kingston", "Paul Deangelo-Willis"]
+
 
 menu_options = ["1) Sign in", "2) Sign out", "3) Quit"]
 
 
 
-def scan_id(option=0):
+def main():
+
+    # THIS IS ALL NEW--------------------
+    ids = {}
+    with open("ids.txt", "r") as ins:
+        for line in ins:
+            (key, val) = line.split()
+            ids[key] = int(val)
+    print(ids)
+    # -------------------------------------
     global name
     name = input("What's your name?")
-    if name in names:
+
+    if name in ids:
+        choice = input("Select your command:\n" + " ".join(menu_options) + "\n")
+        choice = int(choice)
+
         #stuff that was in the log() function
         print("Logging file...")
         log_file = open("log.csv","a")
         time.sleep(0.1)
 
         #this next thing is new
-        if option == 1:
+        if choice == 1:
             log_file.write("\n" + name + ", " + str(datetime.datetime.now()) + ",SIGNED IN")
             print("You are now signed in!")
-        elif option == 2:
+        elif choice == 2:
             log_file.write("\n" + name + ", " + str(datetime.datetime.now()) + " SIGNED OUT")
             print("You are now signed out! Thanks for coming!")
         else:
             time.sleep(0.1)
     else:
         print("Sorry. Please enter a valid name")
-        scan_id()
+        main()
 
-def main():
     #print("Main has been called!")
-    choice = input("Select your command:\n" + " ".join(menu_options) + "\n")
-    choice = int(choice)
-    if choice == 1:
-        scan_id(1)
-    elif choice == 2:
-        scan_id(2)
-    else:
-        time.sleep(0.5)
-    #scan_id()
+
 
 
 if __name__ == "__main__":
