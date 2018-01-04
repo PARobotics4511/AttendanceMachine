a7ca4c47c7e362fb163eba847cbb4021d94b1de8
PA Student <Student@Provnet.local>
2018-01-04 16:35:54 -0600

    begin intergration with pi

---
+++ attendance_pi.py
@@ -0,0 +1,73 @@
+import RPi.GPIO as GPIO
+import SimpleMFRC522
+
+import time
+import datetime
+
+menu_options = ["1) Sign in", "2) Sign out", "3) Quit"]
+
+#testing-----------------------------------------------------------
+reader = SimpleMFRC522.SimpleMFRC522()
+
+#reading/writing
+def write():
+    try:
+        text = raw_input('New data:')
+        print("Now place your tag to write")
+        reader.write(text)
+        print("Written")
+
+    finally:
+        GPIO.cleanup()
+
+def read():
+    try:
+        id, text = reader.read()
+        print(id)
+        print(text)
+        return text
+    finally:
+        GPIO.cleanup()
+#------------------------------------------------------------------
+
+def main():
+
+    # THIS IS ALL NEW--------------------
+    ids = {}
+    with open("ids.csv", "r") as ins:
+        for line in ins:
+            (key, val) = line.split(',')
+            ids[key] = int(val)
+    print(ids)
+    # -------------------------------------
+    global name
+    name = read()
+
+    if name in ids:
+        choice = input("Select your command:\n" + " ".join(menu_options) + "\n")
+        choice = int(choice)
+
+        #stuff that was in the log() function
+        print("Logging file...")
+        log_file = open("log.csv","a")
+        time.sleep(0.1)
+
+        #this next thing is new
+        if choice == 1:
+            log_file.write("\n" + name + ", " + str(datetime.datetime.now()) + ",SIGNED IN")
+            print("You are now signed in!")
+        elif choice == 2:
+            log_file.write("\n" + name + ", " + str(datetime.datetime.now()) + " SIGNED OUT")
+            print("You are now signed out! Thanks for coming!")
+        else:
+            time.sleep(0.1)
+    else:
+        print("Sorry. Please enter a valid name")
+        main()
+
+    #print("Main has been called!")
+
+
+
+if __name__ == "__main__":
+    main()
